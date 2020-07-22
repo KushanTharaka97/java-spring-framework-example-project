c35bd9d15dadff37ad31ce9b5a155cc2d270189d
Kushan Tharaka <tharakakushan@gmail.com>
2020-07-22 10:33:05 +0530

    CREATED: APPLICATION INITIALIZER class bootstrap the dispather servelet make priority to load the dispather make mapping then all the requests are come for that pattern url goes to the dispather servlet.

---
+++ src/main/java/com/test/ApplicationInitializer.java
@@ -0,0 +1,27 @@
+package com.test;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+public class ApplicationInitializer implements WebApplicationInitializer {
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        //bootstrap dispatherservelet
+        AnnotationConfigWebApplicationContext contect = new AnnotationConfigWebApplicationContext();
+
+        ServletRegistration.Dynamic servletRegistration =
+                servletContext.addServlet("mvc", new DispatcherServlet(contect));
+
+
+        //run in priority speed
+        servletRegistration.setLoadOnStartup(1);
+
+        //Mapping
+        servletRegistration.addMapping("/");
+    }
+}
